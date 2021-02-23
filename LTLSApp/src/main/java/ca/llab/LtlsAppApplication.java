d2f181cfef318817565dab7b50eaca7c6860cc8d
landreab <51808399+landreab@users.noreply.github.com>
2021-02-22 19:54:54 -0500

    Add files via upload
    The start of our LTLS Application. A quick test of Hello World to make sure that our Docker Container works, and that the Spring Boot Jar runs on our server via DockerHub.

---
+++ LTLSApp/src/main/java/ca/llab/LtlsAppApplication.java
@@ -0,0 +1,13 @@
+package ca.llab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtlsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtlsAppApplication.class, args);
+	}
+
+}
